9ce032b4d11cbd0e290fd5a16ad3c451660d0a58
sagar jadhav <sagar.jadhav@novat-tekno.com>
2023-11-04 16:19:25 +0530

    created render function common

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,14 +2,19 @@ import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 import userEvent from "@testing-library/user-event";
 
-test("render one row per user", () => {
-  //Render the component
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "same", email: "sam@sam.com" },
   ];
   render(<UserList users={users} />);
-
+  return {
+    users,
+  };
+}
+test("render one row per user", () => {
+  //Render the component
+  renderComponent();
   //Find all the rows in the table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
@@ -17,11 +22,7 @@ test("render one row per user", () => {
   expect(rows).toHaveLength(2);
 });
 test("render the email and name of each user", () => {
-  const users = [
-    { name: "jane", email: "jane@jane.com" },
-    { name: "same", email: "sam@sam.com" },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (const user of users) {
     const name = screen.getByRole("cell", { name: user.name });
